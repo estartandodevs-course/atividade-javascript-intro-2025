64b547815e984dd8f7b303bf66289d27f616eef7
Adriano Teixeira Benevenuto Sampaio <adrianoteixeira1223@gmail.com>
2025-08-25 11:43:18 -0300

    feat: exercícios concluídos

---
+++ exercicio/javascript1.js
@@ -0,0 +1,14 @@
+let num1 = 10;
+let num2 = 5;
+
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let multiplicacao = num1 * num2;
+let divisao = num1 / num2;
+let resto = num1 % num2;
+
+console.log(`Soma: ${soma}`);
+console.log(`Subtração: ${subtracao}`);
+console.log(`Multiplicação: ${multiplicacao}`);
+console.log(`Divisão: ${divisao}`);
+console.log(`Resto: ${resto}`);
