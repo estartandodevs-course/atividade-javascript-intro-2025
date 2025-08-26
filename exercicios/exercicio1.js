70ad5e3f7c88a9367f356ba03525c5448acbd5fc
Bruno Fabbio <brunobiodev@gmail.com>
2025-08-26 16:30:53 -0300

    feat: fiz os quatro primeiros exercicios

---
+++ exercicios/exercicio1.js
@@ -0,0 +1,16 @@
+/* Crie códigos para cada operação aritmética usando duas variáveis (num1 e num2):
+
+Adição (num1 + num2)
+Subtração (num1 - num2)
+Multiplicação (num1 * num2)
+Divisão (num1 / num2)
+Módulo (num1 % num2) */
+
+let num1 = 10;
+let num2 = 5;
+
+console.log("Adição: " + (num1 + num2));
+console.log("Subtração: " + (num1 - num2));
+console.log("Multiplicação: " + (num1 * num2));
+console.log("Divisão: " + (num1 / num2));
+console.log("Módulo: " + (num1 % num2));
