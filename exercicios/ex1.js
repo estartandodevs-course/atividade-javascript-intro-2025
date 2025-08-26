ceba47457c33534f54ef0942fe63dfbb5d45f3eb
thobiaswallace <thobiaswallace@gmail.com>
2025-08-26 05:49:09 -0300

    feat: Adiciona exercicio1

---
+++ exercicios/ex1.js
@@ -0,0 +1,16 @@
+/* Crie códigos para cada operação aritmética usando duas variáveis (num1 e num2): */
+
+var number1 = 5;
+var number2 = 7;
+
+var soma = number1 + number2;
+var subtracao = number1 - number2;
+var multiplicacao = number1 * number2;
+var divisao = number1 / number2;
+var modulo = number1 % number2;
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
